b5109205cc45bb3a05713431255feffecc43698d
Brian Ridings <bdrid001@gmail.com>
2013-03-31 20:19:05 -0400

    Removed extra files

--- test.py
+++
@@ -1,6 +0,0 @@
-from stree import *
-
-print "Castle", string_match("Castle.2009.S05E18.HDTV.x264-LOL.mp4", "Castle")
-print " World", string_match(" Hello World", "Hello Universe")
-print "Castle", string_match("Castle ", "Castle")
-print "World", string_match("Hello 0 Universe", "Hello & Universe")
